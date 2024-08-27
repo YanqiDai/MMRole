0fcedc403c8f3105f0e21e18c8f6c9fde3788ce1
YanqiDai <yanqidai@outlook.com>
2024-08-27 23:03:14 +0800

    Delete

--- eval/RM_review.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-MODEL="mmrole-agent"
-python eval/RM_review.py --input_dir eval/mini_answers/$MODEL --output_dir eval/mini_reviews/$MODEL
-python eval/RM_review.py --input_dir eval/zero_answers/$MODEL --output_dir eval/zero_reviews/$MODEL
